9bcde5e2c7d73bdbcc53acde539f4092ab66f0ca
yoonsueworks <104703817+yoonsueworks@users.noreply.github.com>
2022-09-04 13:45:11 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+/** @format */
+
+const button = document.querySelector("button");
+const body = document.querySelector("body");
+const sliderImages = document.querySelectorAll(".slide-in");
+
+const modeChanger = function () {
+  const clickedClass = "active_mode";
+  body.classList.toggle(clickedClass);
+};
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+button.addEventListener("click", modeChanger);
